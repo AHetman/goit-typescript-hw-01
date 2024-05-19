48975a100d3fdf1cfbead0040ce9e9442e9fbce2
AHetman <101942183+AHetman@users.noreply.github.com>
2024-05-20 00:52:32 +0300

    ts-1

---
+++ src/basic/3.ts
@@ -0,0 +1,3 @@
+let mixedType: string | number;
+type Toggle = "enable" | "disable";
+let myToggle: Toggle;
---
+++ src/generics/4.ts
@@ -0,0 +1,15 @@
+type UserInfo = {
+  name: string;
+  surname: string;
+  email: string;
+  password: string;
+};
+
+function createOrUpdateUser(initialValues: Partial<UserInfo>) {
+  // Оновлення користувача
+}
+
+createOrUpdateUser({
+  email: "user@mail.com",
+  password: "password123",
+});
